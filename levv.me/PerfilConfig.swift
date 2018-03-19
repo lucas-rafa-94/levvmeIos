f0bd03751688227e6c402a809046318eb07bd5b6
lucas dos santos <lucas_rafa_94@hotmail.com>
2018-03-19 19:35:24 -0300

    commit

---
+++ levv.me/PerfilConfig.swift
@@ -0,0 +1,21 @@
+//
+//  PerfilConfig.swift
+//  levv.me
+//
+//  Created by Lucas dos Santos on 3/19/18.
+//  Copyright © 2018 Lucas dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class PerfilConfig {
+    
+    var titulo: String!
+    var imagem: UIImage!
+    
+    init(titulo : String,  imagem: UIImage) {
+        self.titulo = titulo
+        self.imagem = imagem
+    }
+    
+}
